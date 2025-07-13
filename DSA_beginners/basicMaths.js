691a660a684fd9678804d864f7891878969675a5
shreyansh0009 <saurabhsharraf@gmail.com>
2025-07-13 12:46:19 +0530

    day1

--- DSA_beginners/basicMaths.js
+++ DSA_beginners/basicMaths.js
@@ -1,11 +1,12 @@
-// As like other programming languages, when we divide a number it prints only integer part,
-    //  but in js, it's not like that. 
+// As like other programming languages, when we divide a number it    prints only integer part,
+//  but in js, it's not like that.
 
-    let a = 7;
-    let b = 3;
-    console.log(a/b);  // 2.3333333333333335
+let a = 7;
+let b = 3;
+console.log(a / b); // 2.3333333333333335
 
-    // If we want to print only integer part then we need use a function.
-    console.log(Math.floor(a/b));  // 2
-    
-    
+// If we want to print only integer part then we need use a function.
+console.log(Math.floor(a / b)); // 2
+
+
+
